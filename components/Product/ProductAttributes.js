8894e5608f60fba1f7e07ed8f1c417ab6b530e91
Parcelvy-Admin <dsee007business@yahoo.com>
2021-08-23 01:26:54 +0800

    added delete function

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,12 +1,44 @@
-import { Button, Header } from "semantic-ui-react";
+import { Button, Header, Modal } from "semantic-ui-react";
+import { useState } from "react";
+import baseUrl from "../../utils/baseUrl";
+import { useRouter } from "next/dist/client/router";
+import axios from "axios";
 
-function ProductAttributes({ product: { description } }) {
+function ProductAttributes({ product: { description, _id } }) {
+  const [show, setShow] = useState(false);
+  const router = useRouter();
+
+  const deleteHandler = async () => {
+    const url = `${baseUrl}/api/product?_id=${_id}`;
+    await axios.delete(url);
+    router.push('/')
+  };
   return (
     <>
       <Header as="h3">About this product</Header>
       <p>{description}</p>
-      <Button icon='trash alternate outline' content='Delete Product' color='red'
+      <Button
+        icon="trash alternate outline"
+        content="Delete Product"
+        color="red"
+        onClick={() => setShow(!show)}
       />
+      <Modal open={show} dimmer="blurring">
+        <Modal.Header>Confirm Delete</Modal.Header>
+        <Modal.Content>
+          <p>Are you sure you want to delete this product?</p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button content="Cancel" onClick={() => setShow(!show)} />
+          <Button
+            negative
+            icon="trash"
+            labelPosition="right"
+            content="Delete"
+            onClick={deleteHandler}
+          />
+        </Modal.Actions>
+      </Modal>
     </>
   );
 }
--- pages/api/product.js
+++ pages/api/product.js
@@ -1,10 +1,30 @@
 import connectDb from "../../utils/connectDb";
-import Product from '../../models/Product'
+import Product from "../../models/Product";
 
 connectDb();
 
 export default async (req, res) => {
-    const { _id } = req.query;
-  const products = await Product.findOne({ _id: _id });
-  res.status(200).json(products);
+  switch (req.method) {
+    case "GET":
+      await getHandler(req, res);
+      break;
+    case "DELETE":
+      await deleteHandler(req, res);
+      break;
+    default:
+      res.status(405).send(`Method ${res.method} Not Allowed!`);
+      break;
+  }
 };
+
+async function getHandler(req, res) {
+  const { _id } = req.query;
+  const product = await Product.findOne({ _id });
+  res.status(200).json(product);
+}
+
+async function deleteHandler(req, res) {
+  const { _id } = req.query;
+  await Product.findOneAndDelete({ _id });
+  res.status(204).json({});
+}
